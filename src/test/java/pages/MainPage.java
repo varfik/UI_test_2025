07b34fdf336feaf815d8919821a144cf143aef29
varfik <arinavarf@yandex.ru>
2025-07-03 14:00:02 +0300

    1 action - 1 method on MainPage

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -3,71 +3,65 @@ package pages;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.codeborne.selenide.Selenide;
-import tests.TestConfig;
 import pages.elements.*;
+import tests.TestConfig;
 
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
-/** Главная страница сайта rutube.ru **/
-
+/** Главная страница сайта rutube.ru до авторизации **/
 public class MainPage extends BasePage {
-    /* Список кнопок, позволяющих перейти к окну для авторизации */
-    private List<Button> loginButtons;
-
-    /* Первая видимая кнопка для выполнения входа */
-    private Button firstLoginButton;
+    /* Кнопка для перехода к окну входу */
+    private final Button loginButton = Button.byText("Вход и регистрация");
 
     /* Конструктор класса */
     public MainPage() {
         super(MainPage.class, "main");
     }
 
-    /* Иконка канала отображается */
-    public boolean isChannelIconVisible() {
-        Image image = Image.byClass("freyja_char-header-user-menu__avatar");
-        return image.isDisplayed();
+    /* Нажатие на кнопку входа: "Вход и регистрация" */
+    public void clickLoginButton() {
+        loginButton.press();
+    }
+
+    /* Переключение на iframe для ввода данных */
+    public void switchToLoginFrame() {
+        switchTo().frame(0);
+    }
+
+    /* Ввод номера телефона */
+    public void fillPhone() {
+        Input.byId("phone-or-email-login").fill(TestConfig.getPhone());
+    }
+
+    /* Нажатие на кнопку "Продолжить" */
+    public void clickContinue() {
+        Button.byId("submit-login-continue").press();
+    }
+
+    /* Ввод пароля */
+    public void fillPassword() {
+        Input.byId("login-password").fill(TestConfig.getPassword());
+    }
+
+    /* Нажатие на кнопку "Войти" */
+    public void clickLogin() {
+        Button.byId("submit-login").press();
     }
 
     /* Открытие главной страницы */
-    public void open(String url) {
-        Selenide.open(url);
-        $("body").shouldBe(visible);
-        closePopups();
-    }
-
-    /* Атворизация на главной странице */
-    public void login() {
-        clickOnLogin();
-        /* Переключение на iframe для ввода данных */
-        switchTo().frame(0);
-        /* Ввод номера телефона */
-        Input phoneInput = Input.byId("phone-or-email-login");
-        phoneInput.fill(TestConfig.getPhone());
-        Button continueButton = Button.byId("submit-login-continue");
-        continueButton.press(); /* (!) Возможна captcha -> необходима точка остановки */
-        /* Ввод пароля */
-        Input passwordInput = Input.byId("login-password");
-        passwordInput.fill(TestConfig.getPassword());
-        Button loginButton = Button.byId("submit-login");
-        loginButton.press(); /* (!) Возможна captcha -> необходима точка остановки */
-        closePopups();
-    }
-
-    /* Нажатие на кнопку входа */
-    private void clickOnLogin() {
-        loginButtons = List.of(
-                Button.byText("Вход и регистрация"),
-                Button.byText("Войти")
-        );
-        firstLoginButton = Button.findOneVisible(loginButtons);
-        firstLoginButton.press();
+    public static MainPage openMainPage() {
+        return new MainPage();
     }
 
     /* Закрытие всплывающих окон */
-    private void closePopups() {
-        List<Button> closeButtons = List.of(Button.byAriaLabel("Закрыть"), Button.byText("Ок"), Button.byText("Не надо")).stream().filter(Button::isDisplayed).collect(Collectors.toList());
+    public void closePopups() {
+        List<Button> closeButtons = List.of(
+                Button.byAriaLabel("Закрыть"),
+                Button.byText("Ок"),
+                Button.byText("Не надо"))
+                .stream()
+                .filter(Button::isDisplayed)
+                .collect(Collectors.toList());
 
         for (Button button : closeButtons) {
             try {
