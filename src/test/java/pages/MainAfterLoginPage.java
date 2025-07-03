16a4ab0d6c031857134a1fd3ac7bd71483896cac
varfik <arinavarf@yandex.ru>
2025-07-03 16:40:36 +0300

    added elements and methods on MainAfterLoginPage

--- src/test/java/pages/MainAfterLoginPage.java
+++ src/test/java/pages/MainAfterLoginPage.java
@@ -1,11 +1,18 @@
 package pages;
 
+import pages.elements.Button;
 import pages.elements.Image;
+import pages.elements.Input;
 
 /** Главная страница Rutube.ru после авторизации **/
 public class MainAfterLoginPage extends BasePage {
 
     private final Image channelIconImage = Image.byClass("freyja_char-header-user-menu__avatar");
+
+    private final Input searchInput = Input.byClass("wdp-search-line-module__input");
+
+    private final Button searchButton = Button.byAriaLabel("Отправить форму поиска");
+
 
     /* Конструктор класса */
     public MainAfterLoginPage() {
@@ -16,5 +23,19 @@ public class MainAfterLoginPage extends BasePage {
     public boolean isChannelIconVisible() {
         return channelIconImage.isDisplayed();
     }
+
+    /* Ввод запроса в строку поиска */
+    public void fillSearchInput(String searchQuery) {
+        searchInput.fill(searchQuery);
+    }
+
+
+    /* Нажатие на кнопку поиска */
+    public void clickSearchButton() {
+        searchButton.press();
+    }
+
+
+
 
 }
