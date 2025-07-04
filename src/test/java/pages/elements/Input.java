3900baf65494b56ff86f748ae8704f131e87764b
varfik <arinavarf@yandex.ru>
2025-07-04 20:26:44 +0300

    methods such as getValue() and byWrapper added to Input

--- src/test/java/pages/elements/Input.java
+++ src/test/java/pages/elements/Input.java
@@ -7,6 +7,7 @@ public class Input extends BaseElement {
     private static final String ID_XPATH = "//input[@id='%s']";
     private static final String NAME_XPATH = "//input[@name='%s']";
     private static final String CLASS_XPATH = "//input[@class='%s']";
+    private static final String WRAPPER_XPATH = "//div[contains(@class, '%s')]/input";
 
     private Input(String xpath, String param) {
         super(xpath, param);
@@ -30,5 +31,15 @@ public class Input extends BaseElement {
     /* Формирование Xpath по class*/
     public static Input byClass(String class_name) {
         return new Input(CLASS_XPATH, class_name);
+    }
+
+    /* Формирование Xpath по wrapper*/
+    public static Input byWrapper(String wrapper) {
+        return new Input(WRAPPER_XPATH, wrapper);
+    }
+
+    /* Получение значения в поле */
+    public String getValue() {
+        return baseElement.getValue();g
     }
 }
