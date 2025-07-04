774c506954b47053841e480b01d5ba4e6cad2041
varfik <arinavarf@yandex.ru>
2025-07-04 12:27:54 +0300

    comments added to SearchService

--- src/test/java/services/SearchService.java
+++ src/test/java/services/SearchService.java
@@ -3,11 +3,16 @@ package services;
 import pages.MainAfterLoginPage;
 import pages.ResultsOfSearchPage;
 
+/** Сервис поиска по запросу в поисковой строке
+ *  Возвращает страницу с результатами поиска (ResultsOfSearchPage)
+ *      - открытие авторизированной главной страницы
+ *      - ввод запроса для поиска
+ *      - нажатие на кнопку поиска (переход на страницу результатов) **/
 public class SearchService {
     public static ResultsOfSearchPage search(String searchQuery) {
         MainAfterLoginPage mainAfterLoginPage = new MainAfterLoginPage();
         mainAfterLoginPage.fillSearchInput(searchQuery);
         mainAfterLoginPage.clickSearchButton();
-        return new ResultsOfSearchPage();
+        return new ResultsOfSearchPage(searchQuery);
     }
 }
