e8fd2a0caeba992412944ec79dd3e009727bdada
varfik <arinavarf@yandex.ru>
2025-07-04 20:28:58 +0300

    clickShareButton() and getLinkToShare() added ti VideoPage

--- src/test/java/pages/VideoPage.java
+++ src/test/java/pages/VideoPage.java
@@ -1,12 +1,28 @@
 package pages;
 
+import pages.elements.Button;
+import pages.elements.Input;
+
 public class VideoPage extends BasePage {
-    /* Кнопка лайка (Button) */
-    /* Поле для комментария (Input) */
-    /* Кнопка для отправки комментария */
+    /* Кнопка "Поделиться */
+    private final Button shareButton = Button.byText("Поделиться");
+
+    /* Поле в "Поделиться" с ссылкой на видео */
+    private final Input shareInput = Input.byWrapper("inputWrapper");
 
     /* Конструктор класса */
     public VideoPage() {
         super(VideoPage.class, "video");
     }
+
+    /* Нажатие на кнопку "Поделиться" */
+    public void clickShareButton() {
+        shareButton.press();
+    }
+
+    /* Получения ссылки из поля "Поделиться" */
+    public String getLinkToShare() {
+        return shareInput.getValue();
+    }
+
 }
