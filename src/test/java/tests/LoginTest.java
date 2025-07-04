a349a5a7a637dc1d34d940ee65590367aef8efbb
varfik <arinavarf@yandex.ru>
2025-07-04 11:30:38 +0300

    comments added to LoginTest

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,19 +3,23 @@ package tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import pages.MainAfterLoginPage;
-import pages.MainPage;
 import services.AuthService;
 
 public class LoginTest extends BaseTest {
 
-    /* Вывод данных для вход */
+    /** Тест выводит в консоль данные для входа (номер телефона и пароль)
+     *  - получение тестового телефона из конфига (TestConfig.getPhone())
+     *  - получение тестового пароля из конфига (TestConfig.getPassword())
+     *  - вывод значения в стандартный вывод (System.out) **/
     @Test
     void showConfig() {
         System.out.println("Phone: " + TestConfig.getPhone());
         System.out.println("Password: " + TestConfig.getPassword());
     }
 
-    /* Успешное отображение иконки пользователя после входа */
+    /** Тест проверяет успешность авторизации через отображение иконки пользователя
+     * - авторизация пользователя
+     * - проверка видимости иконки канала на главной странице после входа (isChannelIconVisible() = true) **/
     @Test
     public void userIconIsDisplayedAfterSuccessfulLogin() {
         MainAfterLoginPage mainPageAfterLogin = AuthService.auth();
