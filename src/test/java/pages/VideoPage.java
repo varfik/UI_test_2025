76820fb663a35e8353431a5916a25d64c1b26316
varfik <arinavarf@yandex.ru>
2025-07-04 13:47:52 +0300

    little comments on VideoPage

--- src/test/java/pages/VideoPage.java
+++ src/test/java/pages/VideoPage.java
@@ -1,6 +1,10 @@
 package pages;
 
 public class VideoPage extends BasePage {
+    /* Кнопка лайка (Button) */
+    /* Поле для комментария (Input) */
+    /* Кнопка для отправки комментария */
+
     /* Конструктор класса */
     public VideoPage() {
         super(VideoPage.class, "video");
