cac21ecba8662b31eca6cdc4c5f1f23defbf465e
varfik <arinavarf@yandex.ru>
2025-07-04 12:29:34 +0300

    unification for CardModule

--- src/test/java/pages/elements/CardModule.java
+++ src/test/java/pages/elements/CardModule.java
@@ -1,17 +1,17 @@
 package pages.elements;
 
 /** Карточка канала <a> 0**/
-public class ChannelCardModule extends BaseElement {
+public class CardModule extends BaseElement {
     private static final String SPAN_XPATH = "//a[.//span[contains(., '%s')]]";
 
     /* Конструктор класса */
-    private ChannelCardModule(String xpath, String param) {
+    private CardModule(String xpath, String param) {
         super(xpath, param);
     }
 
     /* Формированию Xpath по span */
-    public static ChannelCardModule bySpan(String spanText) {
-        return new ChannelCardModule(SPAN_XPATH, spanText);
+    public static CardModule bySpan(String spanText) {
+        return new CardModule(SPAN_XPATH, spanText);
     }
 
     /* Нажатие на карточку канала */
