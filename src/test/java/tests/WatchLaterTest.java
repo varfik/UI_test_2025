7fc25e1add69b38803773f5d7dad745f638161b3
Екатерина Солдунова <144446182+ekaterina3383@users.noreply.github.com>
2025-07-05 12:21:01 +0300

    Update and rename WatchLaterPage.java to WatchLaterTest.java

--- src/test/java/tests/WatchLaterTest.java
+++ src/test/java/tests/WatchLaterTest.java
@@ -14,16 +14,22 @@ import services.SearchType;
 public class WatchLaterPage extends BaseTest {
     private final String TEST_VIDEO = "Искусственный интеллект и его применение в жизни / Практика 2025 ЛЭТИ 3383";
 
+    /** Тестирование добавления видео в "Смотреть позже" и его удаления:
+    - авторизация
+    - поиск канала по названию тестового видео
+    - открывание меню у найденного видео
+    - добавление видео в "Смотреть позже"
+    - перейти на страницу "Смотреть позже"
+    - проверка наличия видео в списке
+    - удалить видео из "смотреть позже"
+    - проверка отсутствие видео в списке **/
     @Test
     public void testAddAndRemoveFromWatchLater() {
         MainAfterLoginPage mainPageAfterLogin = AuthService.auth();
         ResultsOfSearchPage resultsOfSearchPage = SearchService.search(
                 TEST_VIDEO, SearchType.CHANNEL);
 
-        new VideoCard(TEST_VIDEO)
-                .openMenu()
-                .addToWatchLater();
-
+        VideoCard video = resultsOfSearchPage.openMenu().addToWatchLater();
         WatchLaterPage watchLaterPage = new WatchLaterPage();
         Assertions.assertTrue(watchLaterPage.isVideoPresent(TEST_VIDEO),
                 "Видео не добавилось в 'Смотреть позже'");
