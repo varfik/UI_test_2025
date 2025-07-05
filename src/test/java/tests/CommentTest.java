09353a77477995bc2caab4d943fdde8de62f3035
varfik <arinavarf@yandex.ru>
2025-07-05 17:36:47 +0300

    fixed declaration

--- src/test/java/tests/CommentTest.java
+++ src/test/java/tests/CommentTest.java
@@ -9,47 +9,59 @@ import services.AuthService;
 import services.SearchService;
 import services.SearchType;
 
-/** Комментарии **/
-public class CommentTest extends BaseTest{
-
-    /* Тест проверяет добавление комментария:
-      - авторизация
-      - поиск видео с названием "Космос и его составляющие / Практика 2025 ЛЭТИ 3383"
-      - ввод в поле "Ваш комментарий" строки "Хорошее видео!"
-      - нажатие кнопки "Отправить"
-      - проверка, что введённый комментарий отображается под видео
-     */
+/**
+ * Комментарии
+ * @author Zemerova
+ **/
+public class CommentTest extends BaseTest {
+    /**
+     * Тест проверяет добавление комментария:
+     * - авторизация
+     * - поиск видео с названием "Космос и его составляющие / Практика 2025 ЛЭТИ 3383"
+     * - ввод в поле "Ваш комментарий" строки "Хорошее видео!"
+     * - нажатие кнопки "Отправить"
+     * - проверка, что введённый комментарий отображается под видео
+     **/
     @Test
-    public void AddedCommentIsDisplayed(){
+    public void AddedCommentIsDisplayed() {
         MainAfterLoginPage mainAfterLoginPage = AuthService.auth();
-        ResultsOfSearchPage resultsOfSearchPage = SearchService.search("Космос и его составляющие / Практика 2025 ЛЭТИ 3383", SearchType.VIDEO);
+        ResultsOfSearchPage resultsOfSearchPage = SearchService.search(
+                "Космос и его составляющие / Практика 2025 ЛЭТИ 3383", SearchType.VIDEO);
         VideoPage mainVideoPage = resultsOfSearchPage.clickVideoNameVideoCardModule();
         mainVideoPage.fillCommentInput("Хорошее видео!");
         mainVideoPage.clickSendCommentButton();
 
         boolean commentIsExist = true;
-        try { mainVideoPage.findCommentByText("Хорошее видео!"); }
-        catch (Exception e) { commentIsExist = false; }
+        try {
+            mainVideoPage.findCommentByText("Хорошее видео!");
+        } catch (Exception e) {
+            commentIsExist = false;
+        }
         Assertions.assertTrue(commentIsExist, "Введённый комментарий не отображается!");
     }
 
-    /* Тест проверяет удаление комментария:
-      - авторизация
-      - поиск видео с названием "Космос и его составляющие / Практика 2025 ЛЭТИ 3383"
-      - поиск под видео комментария "Хорошее видео!"
-      - нажатие на кнопку "Троеточие" для комментария
-      - нажатие кнопки "Удалить"
-      - проверка, что удалённый комментарий не отображается под видео
-     */
+    /**
+     * Тест проверяет удаление комментария:
+     * - авторизация
+     * - поиск видео с названием "Космос и его составляющие / Практика 2025 ЛЭТИ 3383"
+     * - поиск под видео комментария "Хорошее видео!"
+     * - нажатие на кнопку "Троеточие" для комментария
+     * - нажатие кнопки "Удалить"
+     * - проверка, что удалённый комментарий не отображается под видео
+     **/
     @Test
-    public void DeletedCommentIsNotDisplayed(){
+    public void DeletedCommentIsNotDisplayed() {
         MainAfterLoginPage mainAfterLoginPage = AuthService.auth();
-        ResultsOfSearchPage resultsOfSearchPage = SearchService.search("Космос и его составляющие / Практика 2025 ЛЭТИ 3383", SearchType.VIDEO);
+        ResultsOfSearchPage resultsOfSearchPage = SearchService.search(
+                "Космос и его составляющие / Практика 2025 ЛЭТИ 3383", SearchType.VIDEO);
         VideoPage mainVideoPage = resultsOfSearchPage.clickVideoNameVideoCardModule();
 
         boolean commentIsDeleted = true;
-        try { mainVideoPage.deleteCommentByText("Хорошее видео!"); }
-        catch (Exception e) { commentIsDeleted = false; }
+        try {
+            mainVideoPage.deleteCommentByText("Хорошее видео!");
+        } catch (Exception e) {
+            commentIsDeleted = false;
+        }
         Assertions.assertTrue(commentIsDeleted, "Введённый комментарий не удалён!");
     }
 }
