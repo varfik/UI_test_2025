6d2238b1dd11308163b1df915d294f1c50b4a11c
varfik <arinavarf@yandex.ru>
2025-07-05 17:47:03 +0300

    fixed declaration

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -8,32 +8,46 @@ import org.junit.jupiter.api.BeforeEach;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
-/** Общий класс для других тестовых классов
- *  - от него наследуются другие классы
- *  - экземпляр класса не создается (abstract) **/
+/**
+ * Общий класс для других тестовых классов
+ * - от него наследуются другие классы
+ * - экземпляр класса не создается (abstract)
+ */
 abstract public class BaseTest {
 
-    /* URL главной страницы сайта */
+    /**
+     * URL главной страницы сайта
+     */
     protected final static String BASE_URL = "https://rutube.ru/";
 
-    /* Выполнение метода перед каждым запуском тестов */
+    /**
+     * Выполнение метода перед каждым запуском тестов
+     */
     @BeforeEach
-    public void init(){
+    public void init() {
         setUp();
         open();
     }
 
-    /* Выполнение метода после каждого закрытия тестов */
+    /**
+     * Выполнение метода после каждого закрытия тестов
+     */
     @AfterEach
-    public void tearDown() { Selenide.closeWebDriver(); }
+    public void tearDown() {
+        Selenide.closeWebDriver();
+    }
 
-    /* Открытие страницы по базовой URL */
+    /**
+     * Открытие страницы по базовой URL
+     */
     protected void open() {
         Selenide.open(BASE_URL);
         $("body").shouldBe(visible);
     }
 
-    /* Установка, настройка, инициализация браузера */
+    /**
+     * Установка, настройка, инициализация браузера
+     */
     private void setUp() {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
